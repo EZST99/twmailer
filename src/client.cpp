e9b0a10c4cce4800786860b93e709d0fe35b363d
ezst99 <ezst99@DESKTOP-PG220UM>
2024-12-10 17:04:22 +0100

    Initial commit

---
+++ src/client.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+// Buffer size definition
+#define BUF 1024
+#define PORT 6543
+
+// Function declarations
+void sendRequest(int client_socket, const std::string& request);
+std::string receiveResponse(int client_socket);
+void handleSend(int client_socket);
+
+int main(int argc, char** argv) {
+    int client_socket;
+    sockaddr_in server_address;
+
+    // Create the socket
+    if ((client_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket creation error");
+        return EXIT_FAILURE;
+    }
+
+    // Server address configuration
+    server_address.sin_family = AF_INET;
+    server_address.sin_port = htons(PORT);
+    if (argc > 1) {
+        inet_aton(argv[1], &server_address.sin_addr);
+    } else {
+        inet_aton("127.0.0.1", &server_address.sin_addr);
+    }
+
+    // Connect to the server
+    if (connect(client_socket, (sockaddr*)&server_address, sizeof(server_address)) == -1) {
+        perror("Connection error");
+        return EXIT_FAILURE;
+    }
+
+    std::cout << "Connection with server established!\n";
+
+    // Buffer for receiving server messages
+    std::string mainBuffer(BUF, '\0');
+    int size = recv(client_socket, &mainBuffer[0], mainBuffer.size(), 0);
+    if (size > 0) {
+        mainBuffer.resize(size);
+        std::cout << mainBuffer;
+    }
+
+    // Main loop for client commands
+    while (true) {
+        std::cout << ">> ";
+        std::string command;
+        std::getline(std::cin, command);
+
+        if (command == "SEND") {
+            handleSend(client_socket);
+        } else {
+            std::cout << "Unknown command. Try SEND, LIST, READ, DEL, or QUIT.\n";
+        }
+    }
+
+    // Close the socket
+    close(client_socket);
+    return EXIT_SUCCESS;
+}
+
+// Function definitions
+
+void sendRequest(int client_socket, const std::string& request) {
+    if (send(client_socket, request.c_str(), request.size(), 0) == -1) {
+        perror("Send error");
+        exit(EXIT_FAILURE);
+    }
+}
+
+std::string receiveResponse(int client_socket) {
+    std::string buffer(BUF, '\0');
+    int size = recv(client_socket, &buffer[0], buffer.size(), 0);
+    if (size == -1) {
+        perror("Receive error");
+        exit(EXIT_FAILURE);
+    } else if (size == 0) {
+        std::cerr << "Server closed the connection.\n";
+        exit(EXIT_FAILURE);
+    }
+    buffer.resize(size);
+    return buffer;
+}
+
+void handleSend(int client_socket) {
+    std::string sender, receiver, subject, message, line;
+
+    std::cout << "Sender: ";
+    std::cin >> sender;
+    std::cout << "Receiver: ";
+    std::cin >> receiver;
+    std::cin.ignore(); // Clear input buffer
+    std::cout << "Subject: ";
+    std::getline(std::cin, subject);
+    std::cout << "Message (end with a single '.' on a new line):\n";
+    while (std::getline(std::cin, line) && line != ".") {
+        message += line + "\n";
+    }
+
+    std::string request = "SEND\n" + sender + "\n" + receiver + "\n" + subject + "\n" + message + ".\n";
+    sendRequest(client_socket, request);
+
+    std::string response = receiveResponse(client_socket);
+    std::cout << "Server: " << response << std::endl;
+}
+
---
+++ src/server.cpp
@@ -0,0 +1,145 @@
+#include <iostream>
+#include <cstring>
+#include <sstream>
+#include <csignal>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <filesystem>
+#include <fstream>
+
+#define BUF 1024
+#define PORT 6543
+
+int abortRequested = 0;
+int create_socket = -1;
+
+void signalHandler(int sig);
+void handleSend(int current_socket, const std::string& sender, const std::string& receiver, const std::string& subject, const std::string& message);
+
+void clientCommunication(int current_socket) {
+    std::string buffer(BUF, '\0');
+
+    std::string welcomeMessage = "Welcome to myserver!\r\nPlease enter your commands...\r\n";
+    send(current_socket, welcomeMessage.c_str(), welcomeMessage.length(), 0);
+
+    do {
+        int size = recv(current_socket, &buffer[0], buffer.size(), 0);
+        if (size > 0) {
+            buffer.resize(size); // Puffergröße anpassen
+            std::istringstream request(buffer);
+            std::string command;
+            request >> command; // Lese den Befehl aus
+
+            if (command == "SEND") {
+                std::string sender, receiver, subject, message, line;
+                request >> sender >> receiver;
+                std::getline(request, subject);
+                while (std::getline(request, line) && line != ".") {
+                    message += line + "\n";
+                }
+                handleSend(current_socket, sender, receiver, subject, message);
+            }
+        } else if (size == 0) {
+            std::cout << "Client closed the connection.\n";
+            break;
+        } else {
+            perror("Recv error");
+            break;
+        }
+    } while (std::string(buffer) != "quit" && !abortRequested);
+
+    shutdown(current_socket, SHUT_RDWR);
+    close(current_socket);
+}
+
+int main() {
+    sockaddr_in address, cliaddress;
+    socklen_t addrlen;
+    int reuseValue = 1;
+
+    signal(SIGINT, signalHandler);
+
+    // Socket erstellen
+    if ((create_socket = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+        perror("Socket error");
+        return EXIT_FAILURE;
+    }
+
+    setsockopt(create_socket, SOL_SOCKET, SO_REUSEADDR, &reuseValue, sizeof(reuseValue));
+    setsockopt(create_socket, SOL_SOCKET, SO_REUSEPORT, &reuseValue, sizeof(reuseValue));
+
+    memset(&address, 0, sizeof(address));
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(create_socket, (sockaddr*)&address, sizeof(address)) == -1) {
+        perror("Bind error");
+        return EXIT_FAILURE;
+    }
+
+    if (listen(create_socket, 5) == -1) {
+        perror("Listen error");
+        return EXIT_FAILURE;
+    }
+
+    while (!abortRequested) {
+        std::cout << "Waiting for connections...\n";
+        addrlen = sizeof(cliaddress);
+        int new_socket = accept(create_socket, (sockaddr*)&cliaddress, &addrlen);
+        if (new_socket == -1) {
+            if (abortRequested) {
+                perror("Accept error after abort");
+            } else {
+                perror("Accept error");
+            }
+            break;
+        }
+
+        std::cout << "Client connected from " << inet_ntoa(cliaddress.sin_addr) 
+                  << ":" << ntohs(cliaddress.sin_port) << "\n";
+        clientCommunication(new_socket);
+    }
+
+    if (create_socket != -1) {
+        shutdown(create_socket, SHUT_RDWR);
+        close(create_socket);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+void signalHandler(int sig) {
+    if (sig == SIGINT) {
+        std::cout << "Abort requested...\n";
+        abortRequested = 1;
+        close(create_socket);
+    }
+}
+
+void handleSend(int client_socket, const std::string& sender, const std::string& receiver, const std::string& subject, const std::string& message) {
+    std::string userDir = "./mail-spool/" + receiver;
+    std::filesystem::create_directories(userDir);
+
+    int messageId = 1;
+    for (const auto& entry : std::filesystem::directory_iterator(userDir)) {
+        messageId++;
+    }
+
+    std::string messageFile = userDir + "/" + std::to_string(messageId) + ".msg";
+    std::ofstream outFile(messageFile);
+    if (outFile) {
+        outFile << "Sender: " << sender << "\n";
+        outFile << "Subject: " << subject << "\n";
+        outFile << "Message:\n" << message << "\n";
+        outFile.close();
+        send(client_socket, "OK\n", 3, 0);
+    } else {
+        send(client_socket, "ERR\n", 4, 0);
+    }
+}
+
+
